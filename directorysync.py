62624fc00925d6a2de27782ac63b1b52dc295358
Grant Montgomery <montg433@d.umn.edu>
2019-11-20 22:39:03 -0600

    more progress

--- directorysync.py
+++ directorysync.py
@@ -1,13 +1,26 @@
 #!/usr/bin/python
+
+import os;
+import sys;
+import subprocess;
 from dirsync import sync
 
-source_path = '/home/lettucebowler/Documents'
+source_path = '/home/lettucebowler/Documents/'
 target_paths = {
+    # '/home/lettucebowler/Documents2/'
     '192.168.122.26:/home/lettucebowler/Documents/',
     '192.168.122.75:/home/lettucebowler/Documents/',
     '192.168.122.82:/home/lettucebowler/Documents/'
 }
 
 for target in target_paths:
-    sync(source_path, target, 'sync')
-    sync(target, source_path, 'sync')
+    # sync(source_path, target, 'sync')
+    # sync(target, source_path, 'sync')
+    cmd = 'dirsync' + " " + source_path + " " + target + " -s -c"
+    process = subprocess.Popen(cmd, shell=True)
+    try:
+        process.wait()
+    except KeyboardInterrupt:
+        process.kill()
+        sys.exit(1)
+        break
