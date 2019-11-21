e53f4d721680e9659f37e5138ee68475b229e4af
Grant Montgomery <montg433@d.umn.edu>
2019-11-21 13:32:54 -0600

    baby it does stuff, just gotta start using ssh keys

--- sync.py
+++ sync.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python
 
-destinations = {
-    #remote sync destinations
-    '192.168.122.75:/home/lettucebowler/Documents/',
-    '192.168.122.82:/home/lettucebowler/Documents/',
-    '192.168.122.26:/home/lettucebowler/Documents/'
-}
+# destinations = {
+#     #remote sync destinations
+#     '192.168.122.75:/home/lettucebowler/Documents/',
+#     '192.168.122.82:/home/lettucebowler/Documents/',
+#     '192.168.122.26:/home/lettucebowler/Documents/'
+# }
 
 src   = '/home/lettucebowler/Documents/'
-rsync = 'rsync --times '
+dest = '192.168.122.166:/home/lettucebowler/Documents/'
 
 # ----------------------------------------------------------
 
@@ -16,24 +16,22 @@ import os;
 import sys;
 import subprocess;
 
-destinations
+#sync files from local folder to server
+cmd1 = "rsync --times -a '" + src + "' '" + dest + "'"
+process1 = subprocess.Popen(cmd1, shell=True)
 
-# Only show progress when we're running in a terminal (and not cron):
-if sys.stdout.isatty():
-    rsync = rsync + '--progress '
+try:
+    process1.wait()
+except KeyboardInterrupt:
+    process1.kill()
+    sys.exit(1)
 
-for dir, dirs, files in os.walk(src):
-    for filename in files:
-        if filename.startswith(".") or filename.endswith(".part"):
-            continue
-        for dest in destinations:
-            fullpath = os.path.join(dir, filename)
-            print "Copying '" + filename + "' to folder '" + dest + "'"
-            cmd = rsync + ' "' + fullpath + '" "' + dest + '/."'
-            process = subprocess.Popen(cmd, shell=True)
-            try:
-                process.wait()
-            except KeyboardInterrupt:
-                process.kill()
-                sys.exit(1)
-                break
+#sync files from server to local folder
+cmd2 = "rsync --times -a '" + dest + "' '" + src + "'"
+process2 = subprocess.Popen(cmd2, shell=True)
+
+try:
+    process2.wait()
+except KeyboardInterrupt:
+    process2.kill()
+    sys.exit(1)
