2fc4d6cd380e29dbfe8325da7cb1071d97991bc3
Grant Montgomery <montg433@d.umn.edu>
2019-11-21 16:12:05 -0600

    autofills username for source path

--- scripts/sync.py
+++ scripts/sync.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python
 
-src   = '~/Documents/'
-dest = 'pebble.lettucebowler.net:/home/shared/Documents/'
-
-# ----------------------------------------------------------
-
 import os;
 import sys;
 import subprocess;
+import getpass
+
+username = getpass.getuser()
+src   = "/home/" + username + "/Documents/"
+dest = 'shared@pebble.lettucebowler.net:/home/shared/Documents/'
 
 #sync files from local folder to server
 cmd1 = "rsync --times -as -e \" ssh -i ~/.ssh/id_rsa\" '" + src + "' '" + dest + "'"
 process1 = subprocess.Popen(cmd1, shell=True)
-
 try:
     process1.wait()
 except KeyboardInterrupt:
@@ -22,7 +21,6 @@ except KeyboardInterrupt:
 #sync files from server to local folder
 cmd2 = "rsync --times -as -e \" ssh -i ~/.ssh/id_rsa\" '" + dest + "' '" + src + "'"
 process2 = subprocess.Popen(cmd2, shell=True)
-
 try:
     process2.wait()
 except KeyboardInterrupt:
