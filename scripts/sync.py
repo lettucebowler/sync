d8764a26850bc1b515314037b303ffb5dbaa3b80
Grant Montgomery <montg433@d.umn.edu>
2019-11-22 12:17:53 -0600

    oopsie

--- scripts/sync.py
+++ scripts/sync.py
@@ -5,7 +5,7 @@ import sys;
 import subprocess;
 
 #sync files from local folder to server
-cmd1 = "rsync --times -as -e \" ssh -i ~/.ssh/Lettucebox\" '" + src + "' '" + dest + "'"
+cmd1 = "rsync --times -as --delete -e \" ssh -i ~/.ssh/Lettucebox\" '" + src + "' '" + dest + "'"
 process1 = subprocess.Popen(cmd1, shell=True)
 try:
     process1.wait()
@@ -14,7 +14,7 @@ except KeyboardInterrupt:
     sys.exit(1)
 
 #sync files from server to local folder
-cmd2 = "rsync --times -as -e \" ssh -i ~/.ssh/Lettucebox\" '" + dest + "' '" + src + "'"
+cmd2 = "rsync --times -as --delete -e \" ssh -i ~/.ssh/Lettucebox\" '" + dest + "' '" + src + "'"
 process2 = subprocess.Popen(cmd2, shell=True)
 try:
     process2.wait()
